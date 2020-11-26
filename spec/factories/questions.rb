888d0cd0b13592b25ef604448bb6f566dd003864
Italo Silveira <italo.menna.silveira@gmail.com>
2020-11-26 20:36:07 -0300

    feat(questions): Create model and migration
    - and factory and spec to this model

---
+++ spec/factories/questions.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :question do
+    statement { "MyText" }
+    text { "MyText" }
+    answer { "MyString" }
+    daily_access { 1 }
+    discipline { "MyString" }
+  end
+end
