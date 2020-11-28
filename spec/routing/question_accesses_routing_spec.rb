33857ba9a1793ed43b09ca94dab742d36f5cae47
Italo Silveira <italo.menna.silveira@gmail.com>
2020-11-28 20:14:19 -0300

    feat(spec): Tests generated automatically

---
+++ spec/routing/question_accesses_routing_spec.rb
@@ -0,0 +1,38 @@
+require "rails_helper"
+
+RSpec.describe QuestionAccessesController, type: :routing do
+  describe "routing" do
+    it "routes to #index" do
+      expect(get: "/question_accesses").to route_to("question_accesses#index")
+    end
+
+    it "routes to #new" do
+      expect(get: "/question_accesses/new").to route_to("question_accesses#new")
+    end
+
+    it "routes to #show" do
+      expect(get: "/question_accesses/1").to route_to("question_accesses#show", id: "1")
+    end
+
+    it "routes to #edit" do
+      expect(get: "/question_accesses/1/edit").to route_to("question_accesses#edit", id: "1")
+    end
+
+
+    it "routes to #create" do
+      expect(post: "/question_accesses").to route_to("question_accesses#create")
+    end
+
+    it "routes to #update via PUT" do
+      expect(put: "/question_accesses/1").to route_to("question_accesses#update", id: "1")
+    end
+
+    it "routes to #update via PATCH" do
+      expect(patch: "/question_accesses/1").to route_to("question_accesses#update", id: "1")
+    end
+
+    it "routes to #destroy" do
+      expect(delete: "/question_accesses/1").to route_to("question_accesses#destroy", id: "1")
+    end
+  end
+end
---
+++ spec/views/question_accesses/index.html.erb_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "question_accesses/index", type: :view do
+  before(:each) do
+    assign(:question_accesses, [
+      QuestionAccess.create!(),
+      QuestionAccess.create!()
+    ])
+  end
+
+  it "renders a list of question_accesses" do
+    render
+  end
+end
